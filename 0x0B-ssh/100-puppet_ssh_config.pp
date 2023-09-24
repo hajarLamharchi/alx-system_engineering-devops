d248bc08221b98160c72a77d5c78c38dbf3a3c44
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-24 13:55:28 +0000

    fixing task4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 # This script sets up ssh configuration using puppet
 
-$str = "Host 370337-web-01
-		HostName 52.86.213.205
-		Port 22
-		User ubuntu
-		IdentityFile /home/vagrant/.ssh/school
-		PasswordAuthentication no"
+file_line {'Turn off passwd auth':
+  path => '/home/ubuntu/.ssh/config',
+  line => 'PasswordAuthentication no'
+}
 
-file {'/home/ubuntu/.ssh/config':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  content => $str
+file_line {'Declare identity file':
+  path => '/home/ubuntu/.ssh/config',
+  line => 'IdentityFile /home/vagrant/.ssh/school'
 }
