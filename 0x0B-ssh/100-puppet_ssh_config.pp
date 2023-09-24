1acd947104acc23827c0a97f98b3efcc9d9ea22b
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-24 13:23:59 +0000

    config with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 # This script sets up ssh configuration using puppet
 
-exec {
+$str = "Host 370337-web-01
+		HostName 52.86.213.205
+		Port 22
+		User ubuntu
+		IdentityFile ~/.ssh/school
+		PasswordAuthentication no"
+
+file {'/home/vagrant/.ssh/config':
+  content => $str
+}
