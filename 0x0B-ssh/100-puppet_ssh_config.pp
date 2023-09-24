0ecd881284a2e76cd60593238b76f9b68ed7a0a6
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-24 13:59:52 +0000

    fixing task4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,13 @@
 # This script sets up ssh configuration using puppet
 
 file_line {'Turn off passwd auth':
-  path => '/home/ubuntu/.ssh/config',
-  line => 'PasswordAuthentication no'
+  ensure => present,
+  path   => '/home/ubuntu/.ssh/config',
+  line   => 'PasswordAuthentication no'
 }
 
 file_line {'Declare identity file':
-  path => '/home/ubuntu/.ssh/config',
-  line => 'IdentityFile /home/vagrant/.ssh/school'
+  ensure => present,
+  path   => '/home/ubuntu/.ssh/config',
+  line   => 'IdentityFile /home/vagrant/.ssh/school'
 }
