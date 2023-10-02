565b34cf5cfdd0bec4504cae5e7d998a56fed129
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-02 23:28:33 +0000

    fix puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,16 @@
 # Creating a custom HTTP header response
 
-$hostname = $::hostname
-
-exec {'update':
-  command  => 'sudo apt-get update',
+exec { 'apt':
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx',
   provider => shell,
 }
 
-package {'nginx':
-  ensure => 'installed',
-}
-
-service {'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
-}
-
-exec {'http header response':
-  command  => "sudo sed -i '/listen 80 default_server;/a add_header X-Served-By $hostname;' /etc/nginx/sites-available/default",
+-> exec { 'configuration':
+  command => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
   provider => shell,
-  require  => Package['nginx'],
-  notify   => Exec['nginx_restart'],
 }
 
-exec {'nginx_restart':
-  command     => 'sudo service nginx restart',
-  provider    => shell,
-  refreshonly => true,
+-> exec { 'nginx':
+  command => 'sudo service nginx restart',
+  provider => shell,
 }
