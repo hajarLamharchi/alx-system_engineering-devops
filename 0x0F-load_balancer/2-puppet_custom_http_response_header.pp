bcd4ab6ef17742675b818960806894f4efdd130e
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-02 20:04:58 +0000

    add header with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,8 +10,9 @@ package {'nginx':
 }
 
 exec {'http header response':
-  command => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By \${HOSTNAME};" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By \${HOSTNAME};" /etc/nginx/sites-available/default',
   provider => shell,
+  require  => Package['nginx'],
 }
 
 exec {'run':
