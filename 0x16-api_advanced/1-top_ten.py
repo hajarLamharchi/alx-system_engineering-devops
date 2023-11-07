1f944f7ea2c9fd8b44ed5dbe4c8ae6beddc70acf
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-11-07 18:31:19 +0000

    task1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""return the top ten hot posts titles"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """return the top ten hot posts titles"""
+    headers = {
+             'User-Agent': 'Your-User-Agent-Name'
+             }
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    response = requests.get(url, headers=headers)
+    try:
+        data = response.json()
+        for post in data['data']['children'][:10]:
+            title = post['data']['title']
+            print(title)
+    except KeyError:
+        print(None)
