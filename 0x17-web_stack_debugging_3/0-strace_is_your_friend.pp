8524a69afc7934e8540cff69f63a1724ce5dc972
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-11-08 19:47:59 +0000

    web stack debugging3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # web stuck debugging using strace, fixing the issue and automate it
 
 file { '/var/www/html/wp-settings.php':
-  ensure  => present,
-  match   => '.*\phpp.*',
-  replace => 's/\phpp/\php/',
+  ensure  => file,
+  content => inline_template("<%= File.read('/var/www/html/wp-settings.php').gsub('.phpp', '.php') %>"),
 }
