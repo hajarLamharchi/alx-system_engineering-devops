5de1537e4c3e513dccbd0c1a8149b3004822bd0d
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-11-08 19:18:54 +0000

    web stack debugging3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# web stuck debugging using strace, fixing the issue and automate it
+
+file_line {'replace':
+  path    => '/var/www/html/wp-settings.php',
+  match   => 'phpp',
+  replace => 'php',
+}
