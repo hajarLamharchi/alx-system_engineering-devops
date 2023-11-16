476945b605b6b9c6aa6ee8ace9ec75841ebdf381
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-11-16 14:59:46 +0000

    task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,12 @@
 # puppet manifest to change the limit of file descriptor
 
-class main {
-  file { '/etc/default/nginx':
-  ensure  => file,
-  content => "ULIMIT='-n 4096'\n",
-  subscribe  => Exec['restart-nginx'],
-  }
-
-  exec { 'restart-nginx':
-    command     => '/usr/bin/sudo /usr/bin/service nginx restart',
-    refreshonly => true,
-  }
+file { '/etc/default/nginx':
+  ensure    => file,
+  content   => "ULIMIT='-n 4096'\n",
+  subscribe => Exec['restart-nginx'],
 }
 
-include main
+exec { 'restart-nginx':
+  command     => '/usr/bin/sudo /usr/bin/service nginx restart',
+  refreshonly => true,
+}
