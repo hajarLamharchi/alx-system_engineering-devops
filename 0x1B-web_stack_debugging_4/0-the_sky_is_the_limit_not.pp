d007aa2a412fccc60798f187ed23235a6962c03c
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-11-16 13:39:10 +0000

    task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,6 @@
 # puppet manifest to change the limit of file descriptor
 
-file_line { 'increase_ulimit':
+file { '/etc/default/nginx':
   ensure  => present,
-  path    => '/etc/default/nginx',
-  match   => '^ULIMIT="-n 15"$',
-  line    => 'ULIMIT="-n 4096"',
+  content => inline_template("# /etc/default/nginx\n<%= 'ulimit -n 4096' %>\n"),
 }
